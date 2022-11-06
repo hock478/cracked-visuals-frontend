2bca3cb28e99c05a7f2fdd0844833b479f4b241f
Dawit Gizaw <dawitgizaw@dawits-mbp.myfiosgateway.com>
2022-11-06 18:35:43 -0500

    changed logo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f73ba239537a97d5"; }
+/******/ 		__webpack_require__.h = function() { return "d37e16d9ec6d3cec"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
